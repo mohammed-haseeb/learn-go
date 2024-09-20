731ff6e33d2b5e0c22e6cfd33110f5b8c8ef0c06
Mohammed Haseeb <mdhaseeb.98@gmail.com>
2024-09-20 12:39:49 +0530

    implement saving to a file

--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 func main() {
 	cards := newDeck()
 
-	hand, remainingCards := deal(cards, 5)
+	// hand, remainingCards := deal(cards, 5)
 
-	hand.print()
-	remainingCards.print()
+	// hand.print()
+	// remainingCards.print()
+
+	cards.saveToFile("my_cards")
 
 }
